6a9151df3ab1354f64d3b4a20ae8ee2df238fa2d
Динар Ганиев <dinar0607s@gmail.com>
2022-04-27 10:32:55 +0300

    'lab4'

--- lab4/app/app.py
+++ lab4/app/app.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 from flask import Flask, render_template, session, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin,  login_user, logout_user, login_required
 from mysql_db import MySQL
+import mysql.connector as connector
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
@@ -17,6 +18,18 @@ app.config.from_pyfile('config.py')
 
 mysql = MySQL(app)
 
+CREATE_PARAMS = ['login', 'password', 'first_name', 'last_name', 'middle_name', 'role_id']
+def request_params(params_list):
+    params = {}
+    for param_name in params_list:
+        params[param_name] = request.form.get(param_name) or None
+    return params
+
+def load_roles():
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute('SELECT * FROM roles;')
+        roles = cursor.fetchall()
+    return roles
 class User(UserMixin):
     def __init__(self, user_id, login):
         super().__init__()
@@ -58,3 +71,32 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/users')
+def users():
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        cursor.execute('SELECT users.*, roles.name AS role_name FROM users LEFT JOIN roles ON users.role_id = roles.id;')
+        users = cursor.fetchall()
+    return render_template('users/index.html', users=users)
+
+@app.route('/users/new')
+def new():
+    return render_template('users/new.html', user={}, roles=load_roles())
+
+@app.route('/users/create', methods=['POST'])
+@login_required
+def create():
+    params = request_params(CREATE_PARAMS)
+    with mysql.connection.cursor(named_tuple=True) as cursor:
+        try: 
+            cursor.execute(
+                ('INSERT INTO users (login, password_hash, last_name, first_name, middle_name, role_id)'
+                'VALUES (%(login)s, SHA2%(password)s, %(last_name)s, %(first_name)s, %(middle_name)s, %(role_id)s);'),
+                params
+            )
+            mysql.connection.commit()
+        except connector.Error:
+            flash('Введены некорректные данные. Ошибка сохранения', 'danger')
+            return render_template('users/new.html', user=params, roles=load_roles())
+    flash(f"Пользлватель {params.get('login')} был успешно создан", 'success')
+    return redirect(url_for('users'))
